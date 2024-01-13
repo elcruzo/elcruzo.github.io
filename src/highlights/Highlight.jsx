897e188c860bffec1f73f2157c349a45868362ac
ElCruzo <ayomideadekoya266@gmail.com>
2024-01-12 22:45:10 -0500

    reverted changes

--- src/highlights/Highlight.jsx
+++ src/highlights/Highlight.jsx
@@ -5,189 +5,186 @@ import { renderTextContent } from "../textUtils";
 import '../App.css';
 
 const TwoColumnGrid = () => {
-  const [secondCellMarginTop, setSecondCellMarginTop] = useState('');
-  const [gridRect, setGridRect] = useState([]);
-  const [cellsRect, setCellsRect] = useState([]);
+const [secondCellMarginTop, setSecondCellMarginTop] = useState('');
+const [gridRect, setGridRect] = useState([]);
+const [cellsRect, setCellsRect] = useState([]);
 //   const [isOpenArray, setIsOpenArray] = useState(Array(jsonData.length).fill(false));
-  const [isOpenArrays, setIsOpenArrays] = useState(Array(jsonData.length).fill([]));
+const [isOpenArrays, setIsOpenArrays] = useState(Array(jsonData.length).fill([]));
+const [showReadMoreButton, setShowReadMoreButton] = useState(false);
 
-  const gridRef = useRef(null);
-  const ref = useRef(null);
-  const listRefs = Array.from({ length: jsonData.length }, () => useRef(null));
+const gridRef = useRef(null);
+const ref = useRef(null);
+const listRefs = Array.from({ length: jsonData.length }, () => useRef(null));
 
-  const calculateRects = () => {
-    requestAnimationFrame(() => {
-      const newGridRect = gridRef.current?.getBoundingClientRect();
-      setGridRect(newGridRect);
+const calculateRects = () => {
+  requestAnimationFrame(() => {
+    const newGridRect = gridRef.current?.getBoundingClientRect();
+    setGridRect(newGridRect);
 
-      const firstCellHeight = gridRef.current?.children[1]?.clientHeight;
-      setSecondCellMarginTop(`${firstCellHeight / 2}px`);
+    const firstCellHeight = gridRef.current?.children[1]?.clientHeight;
+    setSecondCellMarginTop(`${firstCellHeight / 2}px`);
 
-      const rect = gridRef.current?.children
-        ? Array.from(gridRef.current.children).map((child) =>
-            child.getBoundingClientRect()
-          )
-        : [];
-      setCellsRect(rect);
-    });
-  };
+    const rect = gridRef.current?.children
+      ? Array.from(gridRef.current.children).map((child) =>
+          child.getBoundingClientRect()
+        )
+      : [];
+    setCellsRect(rect);
+  });
+};
 
+calculateRects();
+
+useLayoutEffect(() => {
   calculateRects();
+  window.addEventListener('resize', calculateRects);
 
-  
- useLayoutEffect(() => {
-   calculateRects();
-   window.addEventListener('resize', calculateRects);
- 
-   return () => {
-     window.removeEventListener('resize', calculateRects);
-   };
- }, [gridRef]);
-
- useLayoutEffect(() => {
-  listRefs.forEach((listRef, index) => {
-    if (listRef.current) {
-      console.log(`scrollHeight of li[${index}]:`, listRef.current.scrollHeight);
-      console.log(`clientHeight of li[${index}]:`, listRef.current.clientHeight);
-      // Update the isOpenArrays state based on the heights of the <li> elements
-      // You can use the same logic as before to determine whether to show the "Read More" button
-    }
-  });
-}, [listRefs]);
-
-  const lines = cellsRect.flatMap((_, index) => {
-    if (index % 2 === 0 && index > 0 && index <= cellsRect.length - 1) {
-      const startY =
-        cellsRect[index].top +
-        (cellsRect[index - 1].bottom - cellsRect[index].top) / 2 -
-        gridRect.top;
-      const endX = cellsRect[index].left - gridRect.left;
-      return createLine(startY, endX);
-    }
-    return [];
-  });
-
-  const paths = cellsRect.flatMap((_, index) => {
-    if (index % 2 === 0 && index > 0 && index < cellsRect.length - 2) {
-      const startY =
-        cellsRect[index].top +
-        (cellsRect[index - 1].bottom - cellsRect[index].top) / 2 -
-        gridRect.top;
-      const midY =
-        cellsRect[index - 1].bottom +
-        (cellsRect[index + 1].top - cellsRect[index - 1].bottom) / 2 -
-        gridRect.top;
-      const endX = cellsRect[index].left - gridRect.left;
-      return createPath(startY, midY, endX);
-    }
-    return [];
-  });
-
-    const gridContainerStyle = {
-    display: 'grid',
-    gridTemplateColumns: 'repeat(2, 1fr)',
-    gridGap: '5rem', 
-    position: 'relative',
-    maxWidth: '50rem',
-    margin: '0 auto',
+  return () => {
+    window.removeEventListener('resize', calculateRects);
   };
+}, [gridRef]);
 
-    const cellStyle = {
-    backgroundColor: '#e0e0e0',
-    padding: '2.25rem',
-    border: '0.0625rem solid #ccc',
-    overflow: 'hidden',
-    borderRadius: '1.5rem',
-    margin: '0 auto', // Center the cell
-  };
-  
-  const headerStyle = {
-    fontSize: '2rem', 
-    marginBottom: '1rem', 
-  };
-
-
-  const listStyle = {
-    listStyleType: 'disc',
-    WebkitLineClamp: 3,
-    WebkitBoxOrient: 'vertical',
-    overflow: 'hidden',
-    display: '-webkit-box',
+useLayoutEffect(() => {
+  if (ref.current) {
+    console.log('scrollHeight:', ref.current.scrollHeight);
+    console.log('clientHeight:', ref.current.clientHeight);
+    setShowReadMoreButton (ref.current.scrollHeight > ref.current.clientHeight)
   }
+}, [isOpenArrays]);
 
-  const toggleIsOpen = (cellIndex, listItemIndex) => {
-    const newArrays = [...isOpenArrays];
-    newArrays[cellIndex] = [...(newArrays[cellIndex] || [])];
-    newArrays[cellIndex][listItemIndex] = !newArrays[cellIndex][listItemIndex];
-    setIsOpenArrays(newArrays);
-  };
+const lines = cellsRect.flatMap((_, index) => {
+  if (index % 2 === 0 && index > 0 && index <= cellsRect.length - 1) {
+    const startY =
+      cellsRect[index].top +
+      (cellsRect[index - 1].bottom - cellsRect[index].top) / 2 -
+      gridRect.top;
+    const endX = cellsRect[index].left - gridRect.left;
+    return createLine(startY, endX);
+  }
+  return [];
+});
 
-  return (
-    <div ref={gridRef} style={gridContainerStyle} className="container">
-      <svg
+const paths = cellsRect.flatMap((_, index) => {
+  if (index % 2 === 0 && index > 0 && index < cellsRect.length - 2) {
+    const startY =
+      cellsRect[index].top +
+      (cellsRect[index - 1].bottom - cellsRect[index].top) / 2 -
+      gridRect.top;
+    const midY =
+      cellsRect[index - 1].bottom +
+      (cellsRect[index + 1].top - cellsRect[index - 1].bottom) / 2 -
+      gridRect.top;
+    const endX = cellsRect[index].left - gridRect.left;
+    return createPath(startY, midY, endX);
+  }
+  return [];
+});
+
+const gridContainerStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(2, 1fr)',
+  gridGap: '5rem', 
+  position: 'relative',
+  maxWidth: '50rem',
+  margin: '0 auto',
+};
+
+const cellStyle = {
+  backgroundColor: '#e0e0e0',
+  padding: '2.25rem',
+  border: '0.0625rem solid #ccc',
+  overflow: 'hidden',
+  borderRadius: '1.5rem',
+  margin: '0 auto', // Center the cell
+};
+
+const headerStyle = {
+  fontSize: '2rem', 
+  marginBottom: '1rem', 
+};
+
+
+const listStyle = {
+  listStyleType: 'disc',
+  WebkitLineClamp: 3,
+  WebkitBoxOrient: 'vertical',
+  overflow: 'hidden',
+  display: '-webkit-box',
+}
+
+const toggleIsOpen = (cellIndex, listItemIndex) => {
+  const newArrays = [...isOpenArrays];
+  newArrays[cellIndex] = [...(newArrays[cellIndex] || [])];
+  newArrays[cellIndex][listItemIndex] = !newArrays[cellIndex][listItemIndex];
+  setIsOpenArrays(newArrays);
+};
+
+return (
+  <div ref={gridRef} style={gridContainerStyle} className="container">
+    <svg
+      style={{
+        position: 'absolute',
+        zIndex: -1,
+      }}
+      width="100%"
+      height="100%"
+      fill="none"
+    >
+      {lines}
+      {paths}
+    </svg>
+    {jsonData.map((item, cellIndex) => (
+      <div
+        key={cellIndex}
         style={{
-          position: 'absolute',
-          zIndex: -1,
+          ...cellStyle,
+          gridRow: cellIndex === 0 ? 'span 1' : 'span 2',
+          marginTop: cellIndex === 1 ? secondCellMarginTop : '',
         }}
-        width="100%"
-        height="100%"
-        fill="none"
+        className="mb-4"
       >
-        {lines}
-        {paths}
-      </svg>
-      {jsonData.map((item, cellIndex) => (
-        <div
-          key={cellIndex}
-          style={{
-            ...cellStyle,
-            gridRow: cellIndex === 0 ? 'span 1' : 'span 2',
-            marginTop: cellIndex === 1 ? secondCellMarginTop : '',
-          }}
-          className="mb-4"
-        >
-          <h2 style={headerStyle}>{item.header}</h2>
-          <p>{item.date}</p>
-          <ul className='bulletpoints' ref={ref}>
-            <li><span style={isOpenArrays[cellIndex]?.[0] ? null : listStyle}>{renderTextContent(item.list1)}</span></li>
-            {console.log(item.list.length())}
-            <button onClick={() => toggleIsOpen(cellIndex, 0)} className='readmore-button'>
-                <strong>{isOpenArrays[cellIndex]?.[0] ? 'Read Less' : 'Read More'}</strong>
-            </button>
+        <h2 style={headerStyle}>{item.header}</h2>
+        <p>{item.date}</p>
+        <ul className='bulletpoints'>
+          <li><span style={isOpenArrays[cellIndex]?.[0] ? null : listStyle} ref={listRefs[cellIndex]}>{renderTextContent(item.list1)}</span></li>
+          {showReadMoreButton && (
+          <button onClick={() => toggleIsOpen(cellIndex, 0)} className='readmore-button'>
+              <strong>{isOpenArrays[cellIndex]?.[0] ? 'Read Less' : 'Read More'}</strong>
+          </button>
+          )}
 
+          {item.list2 && (
+          <>
+          <li><span style={isOpenArrays[cellIndex]?.[1] ? null : listStyle} ref={listRefs[cellIndex + 1]}>{renderTextContent(item.list2)}</span></li>
+          {showReadMoreButton && (
+          <button onClick={() => toggleIsOpen(cellIndex, 1)} className='readmore-button'>
+              <strong>{isOpenArrays[cellIndex]?.[1] ? 'Read Less' : 'Read More'}</strong>
+          </button>
+          )}
+          </>)}
 
-            {item.list2 && (
-            <>
-            <li><span style={isOpenArrays[cellIndex]?.[1] ? null : listStyle}>{renderTextContent(item.list2)}</span></li>
-
-            <button onClick={() => toggleIsOpen(cellIndex, 1)} className='readmore-button'>
-                <strong>{isOpenArrays[cellIndex]?.[1] ? 'Read Less' : 'Read More'}</strong>
-            </button>
-
-            </>)}
-
-            {item.list3 && (
-            <>
-            <li><span style={isOpenArrays[cellIndex]?.[2] ? null : listStyle}>{renderTextContent(item.list3)}</span></li>
-
-            <button onClick={() => toggleIsOpen(cellIndex, 2)} className='readmore-button'>
-                <strong>{isOpenArrays[cellIndex]?.[2] ? 'Read Less' : 'Read More'}</strong>
-            </button>
-  
-            </>)}
-          </ul>
-        </div>
-      ))}
-    </div>
-  );
+          {item.list3 && (
+          <>
+          <li><span style={isOpenArrays[cellIndex]?.[2] ? null : listStyle} ref={listRefs[cellIndex + 2]}>{renderTextContent(item.list3)}</span></li>
+          {showReadMoreButton && (
+          <button onClick={() => toggleIsOpen(cellIndex, 2)} className='readmore-button'>
+              <strong>{isOpenArrays[cellIndex]?.[2] ? 'Read Less' : 'Read More'}</strong>
+          </button>
+          )}
+          </>)}
+        </ul>
+      </div>
+    ))}
+  </div>
+);
 };
 
 function Highlight() {
-  return (
-    <div className="highlight-card">
-      <TwoColumnGrid />
-    </div>
-  );
+return (
+  <div className="highlight-card">
+    <TwoColumnGrid />
+  </div>
+);
 }
 
 export default Highlight;

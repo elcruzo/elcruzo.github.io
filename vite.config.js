af64d35770c8d992c2f08c393df70459c8580091
ElCruzo <ayomideadekoya266@gmail.com>
2024-05-16 18:32:43 -0400

    Added 404 error file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/"
+  base: "/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          '404.html': ['./404.html'], // Copy 404.html to the root folder
+        },
+      },
+    },
+  },
 })

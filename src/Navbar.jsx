11465615627bd531330d83c6eccbfb3a47250950
ElCruzo <ayomideadekoya266@gmail.com>
2024-05-16 17:27:41 -0400

    Added active class

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,7 @@
 import {React, useRef} from 'react';
-import './App.css';
 import {FaBars, FaTimes} from "react-icons/fa";
+import { NavLink } from 'react-router-dom';
+import './App.css';
 
 function Navbar() {
     const navRef = useRef();
@@ -11,17 +12,19 @@ function Navbar() {
 
     return (
         <header className='header-nav'>
-            <a href="/" className="logo" title='elcruzo_logo'><span className='alt-color'>El</span>Cruzo</a>
+            <NavLink to="/" className="logo" title='elcruzo_logo'>
+                <span className='alt-color'>El</span>Cruzo
+            </NavLink>
             <nav className='nav navbar navbar-expand-sm' ref={navRef}>
                 <ul className='navbar-nav ml-auto'>
                     <li className="nav-item">
-                        <a className="nav-link" href="/aboutme">About Me</a>
+                        <NavLink className="nav-link" to="/" activeClassName="active">About Me</NavLink>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="https://docs.google.com/document/d/19xVMNChFc0ccuvewNZunGhgSB8Pooy_y7fTLMMFtXCo/edit?usp=sharing">Resume</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/projects">Projects</a>
+                        <NavLink className="nav-link" to="/projects" activeClassName="active">Projects</NavLink>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="http://www.linkedin.com/in/elcruzo/">Contact</a>

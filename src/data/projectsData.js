977e857ed1e4fb394fbe255f52f70343abc1940a
ElCruzo <ayomideadekoya266@gmail.com>
2024-05-17 10:31:33 -0400

    Update Projects page

---
+++ src/data/projectsData.js
@@ -0,0 +1,18 @@
+const projectsData = [
+    {
+      id: 1,
+      image: 'path/to/image1.jpg',
+      description: 'Description of project 1',
+      githubLink: 'https://github.com/user/project1'
+    },
+    {
+      id: 2,
+      image: 'path/to/image2.jpg',
+      description: 'Description of project 2',
+      githubLink: 'https://github.com/user/project2'
+    },
+    // Add more projects as needed
+  ];
+  
+  export default projectsData;
+  
